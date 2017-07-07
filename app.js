c1e75c0520665d5d4978a0324c1326b8b051ee9f
Darnell Gillead <dlgillead@gmail.com>
2017-07-07 11:42:47 -0500

    added logic to check passwords

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ $(function() {
   $("#formData").submit(function(e) {
 
     let url = "http://putsreq.com/Ly0tytjC8430Q0p7Kria";
+    let password = $("#pwd").val();
+    let confirmPwd = $("#confirmPwd").val();
 
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#formData").serialize(),
-           success: function(data)
-           {
-               $("#formData")[0].reset();
-               window.alert("Thank You!");
-           }
-         });
-
-    e.preventDefault();
+    if (password === confirmPwd) {
+      $.ajax({
+             type: "POST",
+             url: url,
+             data: $("#formData").serialize(),
+             success: function(data)
+             {
+                 $("#formData")[0].reset();
+                 window.alert("Thank You!");
+             }
+           });
+    } else {
+      alert("Passwords do not match, please try again.")
+    }
+  e.preventDefault();
   });
 });
